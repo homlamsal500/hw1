48fc94caba7d8b65f3901bb6af232729b6582441
Hom Lamsal <hom.geek005@gmail.com>
2019-08-08 14:32:28 -0500

    c

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hw1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int firstno;
+                int secondno;
+                int sumVal;
+                int subVal;
+                int multVal;
+                double divVal;
+                int remVal;
+
+                Console.WriteLine("Enter first no : ");
+                firstno = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter second no: ");
+                secondno = Convert.ToInt32(Console.ReadLine());
+                sumVal = firstno + secondno;
+                Console.WriteLine("Addition of two no is: " + sumVal);
+                subVal = firstno - secondno;
+                Console.WriteLine("Substraction of two no is: " + subVal);
+                multVal = firstno * secondno;
+                Console.WriteLine("Multiplication  of two no is: " + multVal);
+                divVal = (double)firstno / secondno;
+                Console.WriteLine("Division  of two no is: " + divVal);
+                remVal = firstno % secondno;
+                Console.WriteLine("Remender of  of two no is: " + remVal);
+                Console.ReadLine();
+
+            }
+
+        }
+    }
+}
